7be97f739496c9c7cad18e2fa391a65781d7fcfd
Josh Gao <jmgao@google.com>
2016-04-27 14:41:16 -0700

    ndk-gdb.py: unify --launch and --launch-activity.
    Also clarify the usage text.
    
    Bug: https://github.com/android-ndk/ndk/issues/63
    Change-Id: I05e1186549aa78a52a3a2cdb5bf2adffa81c213f

--- ndk-gdb.py
+++ ndk-gdb.py
@@ -76,21 +76,19 @@ class ArgumentParser(gdbrunner.ArgumentParser):
 
         start_group.add_argument(
             "--attach", nargs='?', dest="package_name", metavar="PKG_NAME",
-            help="attach to application. PKG_NAME to attach to can optionally be specified.")
+            help="attach to application (default)\n"
+                 "autodetects PKG_NAME if not specified")
 
+        # NB: args.launch can be False (--attach), None (--launch), or a string
         start_group.add_argument(
-            "--launch", action="store_true", dest="launch",
-            help="launch application activity (defaults to main activity, "
-                 "configurable with --launch-activity)")
+            "--launch", nargs='?', dest="launch", default=False,
+            metavar="ACTIVITY",
+            help="launch application activity\n"
+                 "launches main activity if ACTIVITY not specified")
 
         start_group.add_argument(
             "--launch-list", action="store_true",
             help="list all launchable activity names from manifest")
-
-        app_group.add_argument(
-            "--launch-activity", action="store", metavar="ACTIVITY",
-            dest="launch_target", help="launch specified application activity")
-
 
         debug_group = self.add_argument_group("debugging options")
         debug_group.add_argument(
@@ -253,25 +251,25 @@ def parse_manifest(args):
 
 
 def select_target(args):
-    assert args.launch
+    assert args.launch != False
+
     if len(args.activities) == 0:
         error("No launchable activities found.")
 
-    if args.launch_target is None:
-        args.launch_target = args.activities[0]
+    if args.launch is None:
+        target = args.activities[0]
 
         if len(args.activities) > 1:
             print("WARNING: Multiple launchable activities found, choosing"
                   " '{}'.".format(args.activities[0]))
     else:
-        canonicalize = canonicalize_activity(args.package_name)
-        activity_name = canonicalize(args.launch_target)
+        activity_name = canonicalize_activity(args.package_name, args.launch)
 
         if activity_name not in args.activities:
             msg = "Could not find launchable activity: '{}'."
             error(msg.format(activity_name))
-        args.launch_target = activity_name
-    return args.launch_target
+        target = activity_name
+    return target
 
 
 @contextlib.contextmanager
@@ -600,8 +598,8 @@ def main():
     if args.launch is False:
         log("Attaching to existing application process.")
     else:
-        launch_target = select_target(args)
-        log("Selected target activity: '{}'".format(launch_target))
+        args.launch = select_target(args)
+        log("Selected target activity: '{}'".format(args.launch))
 
     abi = fetch_abi(args)
 
@@ -633,7 +631,7 @@ def main():
         am_cmd = ["am", "start"]
         if not args.nowait:
             am_cmd.append("-D")
-        component_name = "{}/{}".format(pkg_name, launch_target)
+        component_name = "{}/{}".format(pkg_name, args.launch)
         am_cmd.append(component_name)
         log("Launching activity {}...".format(component_name))
         (rc, _, _) = device.shell_nocheck(am_cmd)
