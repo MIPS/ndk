777ff292e3f007803191566b8f9456ccbabb6aa0
Dan Albert <danalbert@google.com>
2016-06-08 14:20:57 -0700

    Fix standalone toolchains for the new libc++abi.
    We no longer ship libunwind (we only ever did to support
    `LIBCXX_FORCE_REBUILD`, which is being dropped), so don't try to copy
    the headers.
    
    The directories have been flattened. llvm-libc++abi is now libc++abi
    rather than having it tucked away in a libcxxabi subdirectory.
    
    Bug: http://b/19149083
    Bug: https://github.com/android-ndk/ndk/issues/106
    Change-Id: I40acf9f97203791150b39665b24f4f69b0b94406

--- build/tools/make_standalone_toolchain.py
+++ build/tools/make_standalone_toolchain.py
@@ -398,18 +398,16 @@ def create_toolchain(install_path, arch, gcc_path, clang_path, sysroot_path,
 
         # I have no idea why we need this, but the old one does it too.
         copy_directory_contents(
-            os.path.join(libcxxabi_dir, 'libcxxabi/include'),
+            os.path.join(libcxxabi_dir, 'include'),
             os.path.join(install_path, 'include/llvm-libc++abi/include'))
 
         headers = [
             'cxxabi.h',
             '__cxxabi_config.h',
-            'libunwind.h',
-            'unwind.h',
         ]
         for header in headers:
             shutil.copy2(
-                os.path.join(libcxxabi_dir, 'libcxxabi/include', header),
+                os.path.join(libcxxabi_dir, 'include', header),
                 os.path.join(cxx_headers, header))
 
         for abi in get_abis(arch):
