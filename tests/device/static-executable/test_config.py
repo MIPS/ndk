d033163efa48716c85de3b1e1f387babd1ee1866
Dan Albert <danalbert@google.com>
2016-06-27 23:26:09 -0700

    Fix detection of single source crtbegin.
    Before this, if an older platform level had separate source files for
    crtbegin_dynamic.o and crtbegin_static.o and a newer platform level
    had a shared source file, the shared source would not be used for the
    static version because the destination directory would have been
    pre-populated by the old version.
    
    Test: Together with the other CLs in the topic, built the ARM NDK and
          ran tests/device/static-executable with and without
          `--platform android-21`.
    Bug: https://github.com/android-ndk/ndk/issues/132
    
    Change-Id: I8d656076a64361935c427d6cbb7f7f418db8af21

--- tests/device/static-executable/test_config.py
+++
@@ -1,5 +0,0 @@
-def match_broken(abi, platform, device_platform, toolchain, subtest=None):
-    bug = 'https://github.com/android-ndk/ndk/issues/132'
-    if platform >= 21:
-        return 'android-{}'.format(platform), bug
-    return None, None
