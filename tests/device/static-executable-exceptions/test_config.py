0e75a4b85523fc597305d7076b1f17ba2a5affd1
Dan Albert <danalbert@google.com>
2016-06-27 23:43:19 -0700

    Fork static executable test into two.
    The static-executable test couldn't have caught
    https://github.com/android-ndk/ndk/issues/132 because it's already
    marked broken for a different issue (duplicate symbols caused by L's
    libc.a including new/delete and friends colliding with libsupc++ for
    exception support).
    
    To make our test suite marginally less useless, split this into two
    tests. static-executable will be the bare minimum static executable
    test, and static-executable-exceptions will make sure exception
    support works (or, for now, doesn't).
    
    Test: Applied with other patches in topic (one of which removes the
          broken annotation for static-executable), static-executable
          passes for ARM android-21 and static-executable-exceptions
          remains a known failure.
    Bug: http://b/24468267
    Bug: https://github.com/android-ndk/ndk/issues/132
    
    Change-Id: Ib83c8f01974db5139ab4069996a6bf2187e51a94

--- tests/device/static-executable-exceptions/test_config.py
+++ tests/device/static-executable-exceptions/test_config.py
@@ -1,5 +1,4 @@
 def match_broken(abi, platform, device_platform, toolchain, subtest=None):
-    bug = 'https://github.com/android-ndk/ndk/issues/132'
     if platform >= 21:
-        return 'android-{}'.format(platform), bug
+        return 'android-{}'.format(platform), 'http://b/24468267'
     return None, None
