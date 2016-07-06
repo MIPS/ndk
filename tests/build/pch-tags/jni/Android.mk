770f4b14162219895ec3a34ada03dfe789060f8c
Dan Albert <danalbert@google.com>
2016-07-06 15:12:29 -0700

    Fix PCH tagging when LOCAL_ARM_NEON is true.
    Our PCH tagging was correctly handling `LOCAL_ARM_NEON`, but we
    forgot about `LOCAL_ARM_MODE`. Fix the build system and extend the
    test to cover this case.
    
    Test: Added additional test cases to build/pch-tags to cover cases
          where arm/neon tagged sources are the default for the module.
    Bug: https://github.com/android-ndk/ndk/issues/149
    
    Change-Id: I08aef8de4e88f1c6b33024820c4786f9f0031d6e

--- tests/build/pch-tags/jni/Android.mk
+++ tests/build/pch-tags/jni/Android.mk
@@ -3,14 +3,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := pch
 LOCAL_PCH := stdafx.h
-LOCAL_SRC_FILES := foo.cpp bar.cpp
+LOCAL_SRC_FILES := untagged.cpp maybe_tagged.cpp
 LOCAL_CFLAGS := -Werror
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := pch-arm
 LOCAL_PCH := stdafx.h
-LOCAL_SRC_FILES := foo.cpp bar.cpp.arm
+LOCAL_SRC_FILES := untagged.cpp maybe_tagged.cpp.arm
 LOCAL_CFLAGS := -Werror
 include $(BUILD_SHARED_LIBRARY)
 
@@ -20,7 +20,29 @@ ifneq ($(TARGET_ARCH_ABI),armeabi)
 include $(CLEAR_VARS)
 LOCAL_MODULE := pch-neon
 LOCAL_PCH := stdafx.h
-LOCAL_SRC_FILES := foo.cpp bar.cpp.neon
+LOCAL_SRC_FILES := untagged.cpp maybe_tagged.cpp.neon
 LOCAL_CFLAGS := -Werror
+include $(BUILD_SHARED_LIBRARY)
+endif
+
+# We also need to check that we get the PCH tags right for the defaults.
+# See https://github.com/android-ndk/ndk/issues/149.
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := pch-local-arm-mode
+LOCAL_PCH := stdafx.h
+LOCAL_SRC_FILES := untagged.cpp
+LOCAL_CFLAGS := -Werror
+LOCAL_ARM_MODE := arm
+include $(BUILD_SHARED_LIBRARY)
+
+# Another neon test. Can't be built for armeabi.
+ifneq ($(TARGET_ARCH_ABI),armeabi)
+include $(CLEAR_VARS)
+LOCAL_MODULE := pch-local-arm-neon
+LOCAL_PCH := stdafx.h
+LOCAL_SRC_FILES := untagged.cpp
+LOCAL_CFLAGS := -Werror
+LOCAL_ARM_NEON := true
 include $(BUILD_SHARED_LIBRARY)
 endif
