19cf33fcac81da7f88001e05cc738cdc3e83d326
Dan Albert <danalbert@google.com>
2016-07-12 14:13:53 -0700

    Mark build/standalone-toolchain broken for GCC.
    GCC's default include ordering is wrong, preventing the C++ stdlib
    from overriding compiler headers. In this case we can't include
    <cstddef> because the include_next can't find the compiler's header.
    
    Test: Ran the test with both GCC and Clang. GCC reports known
          failure, Clang passes.
    Bug: http://b/30096326
    
    Change-Id: I691991e205e7765511c40a5f5458239e1ebf3c1c

--- tests/build/standalone-toolchain/test_config.py
+++ tests/build/standalone-toolchain/test_config.py
@@ -3,6 +3,15 @@ from __future__ import absolute_import
 import platform
 
 
+def match_broken(abi, api_level, toolchain):
+    if toolchain == '4.9':
+        # GCC's default include ordering is wrong, preventing the C++ stdlib
+        # from overriding compiler headers. In this case we can't include
+        # <cstddef> because the include_next can't find the compiler's header.
+        return toolchain, 'http://b/30096326'
+    return None, None
+
+
 def match_unsupported(_abi, _api_level, _toolchain, _subtest=None):
     if platform.system() == 'Windows':
         # make-standalone-toolchain.sh isn't supported on Windows since we
