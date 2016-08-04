798c9064a0d32744f6c9ec3b8cc6a224322d7388
Dan Albert <danalbert@google.com>
2016-08-04 10:23:17 -0700

    Always build the vulkan test.
    `match_unsupported` is invoked multiple times for device tests. Once
    with `subtest=None` to check if we even support building this project,
    and then once for each executable within the test to see if we support
    running the test for this configuration.
    
    We can always build this test regardless of what device we have
    attached, so gate the check on `subtest != None`.
    
    Change-Id: Icf08aa148ea8a602c6b7dc14f9e8c500831e0378

--- tests/device/test-vulkan/test_config.py
+++ tests/device/test-vulkan/test_config.py
@@ -1,6 +1,10 @@
 def match_unsupported(abi, platform, device_platform, toolchain, subtest=None):
+    # Vulkan isn't supported on armeabi
+    if abi == 'armeabi':
+        return abi
+
     # Vulkan support wasn't added until android-24
-    if device_platform < 24:
+    if device_platform < 24 and subtest != None:
         return device_platform
 
     return None
