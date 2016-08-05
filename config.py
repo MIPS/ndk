ec5f02308887c0ae47fae7620533645bf16f7721
Dan Albert <danalbert@google.com>
2016-08-04 17:12:28 -0700

    Actually name the release with canary.
    I said I did this in
    https://android-review.googlesource.com/#/c/252662/, but that was
    undone as part of my "cleanup".
    
    The package name is still just the build number. This just names the
    extracted directory something like "android-ndk-r13-canary".
    
    Change-Id: Ibae6d312a3aaad4955240f614c64af6a2ab8cc47

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ hotfix_str = chr(ord('a') + hotfix) if hotfix else ''
 beta = 1
 beta_str = '-beta{}'.format(beta) if beta > 0 else ''
 canary = True
-release = 'r{}{}{}'.format(major, hotfix_str, beta_str)
+canary_str = '-canary' if canary else ''
+release = 'r{}{}{}{}'.format(major, hotfix_str, beta_str, canary_str)
