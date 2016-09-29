50e79d73ceabf8ffef6429ebf1e9b28e2181c67d
Dan Albert <danalbert@google.com>
2016-09-29 13:11:46 -0700

    Add -mstackrealign for x86 targets pre-L.
    Test: ./run_tests.py --filter mstackrealign
    Bug: http://b.android.com/222239
    Change-Id: I7692c6ae152676b64386b89198c2b94ab01d3c74
    (cherry picked from commit af6eb78a1320d576492cff6479f46ea2d2e1828c)

---
+++ tests/build/mstackrealign/test.py
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Check for mstackrealign use for old x86 targets.
+
+http://b.android.com/222239 reports that old x86 targets have stack alignment
+issues. For these devices, verify that mstackrealign is used.
+"""
+import os
+import subprocess
+
+
+def run_test(abi=None, platform=None, toolchain=None, build_flags=None):
+    """Checks ndk-build V=1 output for mstackrealign flag."""
+    if build_flags is None:
+        build_flags = []
+
+    ndk_dir = os.environ['NDK']
+    ndk_build = os.path.join(ndk_dir, 'ndk-build')
+    project_path = 'project'
+    ndk_args = build_flags + [
+        'APP_ABI=' + abi,
+        'APP_PLATFORM=android-{}'.format(platform),
+        'NDK_TOOLCHAIN_VERSION=' + toolchain,
+        'V=1',
+    ]
+    proc = subprocess.Popen([ndk_build, '-C', project_path] + ndk_args,
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out, _ = proc.communicate()
+    if proc.returncode != 0:
+        return proc.returncode == 0, out
+
+    out_words = out.split(' ')
+    if abi == 'x86' and platform < 21:
+        result = '-mstackrealign' in out_words
+    else:
+        result = '-mstackrealign' not in out_words
+
+    return result, out
