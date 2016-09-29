2c880e031ea0d0c0ec76128d3473aa6561400530
Dan Albert <danalbert@google.com>
2016-09-29 10:47:21 -0700

    Fix NDK_ANALYZE.
    These paths all bitrotted back when we moved ndk-build into build as
    part of the modular NDK work.
    
    Test: ./run_tests.py --filter NDK_ANALYZE
    Bug: None
    Change-Id: I49ee0f646776efe4237aa03bcfefc2a919dbf647

---
+++ tests/build/NDK_ANALYZE/test.py
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import subprocess
+
+
+def run_test(abi=None, platform=None, toolchain=None,
+             build_flags=None):
+    """Runs the static analyzer on a sample project."""
+    if build_flags is None:
+        build_flags = []
+
+    ndk_dir = os.environ['NDK']
+    ndk_build = os.path.join(ndk_dir, 'ndk-build')
+    project_path = 'project'
+    ndk_args = build_flags + [
+        'APP_ABI=' + abi,
+        'APP_PLATFORM=android-{}'.format(platform),
+        'NDK_ANALYZE=1',
+    ]
+    proc = subprocess.Popen([ndk_build, '-C', project_path] + ndk_args,
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out, _ = proc.communicate()
+    return proc.returncode == 0, out
---
+++ tests/build/NDK_ANALYZE/test_config.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+import sys
+
+
+def build_unsupported(_abi, _api, toolchain):
+    if sys.platform == 'win32':
+        return sys.platform
+    if toolchain != 'clang':
+        return toolchain
+    return None
