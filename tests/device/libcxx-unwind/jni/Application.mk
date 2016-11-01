d84266c360b1fe0fa243146ce07356d49c767c52
Dan Albert <danalbert@google.com>
2016-11-01 07:22:01 -0700

    Fix test configuration, remove known issue.
    Setting the `APP_PLATFORM` for this test to android-21 was preventing
    it from being run on any of the older devices. Turns out this test
    has actually been passing since r12 (it was added early in r12, I
    guess I fixed it some time later in the cycle).
    
    Test: ./validate.py --filter libcxx-unwind # Checked r10 through r14.
    Bug: http://b/28044141
    Change-Id: I12e44899db32cd5052e7ded39a771cdf6239ff48

--- tests/device/libcxx-unwind/jni/Application.mk
+++ tests/device/libcxx-unwind/jni/Application.mk
@@ -1,4 +1,3 @@
 APP_STL := c++_shared
 APP_ABI := armeabi-v7a
-APP_PLATFORM := android-21
 NDK_TOOLCHAIN_VERSION := clang
--- tests/device/libcxx-unwind/test_config.py
+++
@@ -1,4 +0,0 @@
-def run_broken(abi, device_api, toolchain, subtest):
-    if abi.startswith('armeabi') and device_api <= 10 and subtest == 'foo':
-        return '{} {}'.format(abi, device_api), 'http://b/28044141'
-    return None, None
