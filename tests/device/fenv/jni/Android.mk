a5373198ae78500040f8ff468b017e740e59423d
Dan Albert <danalbert@google.com>
2016-11-07 11:27:24 -0800

    Revert "Use libandroid_support for fenv."
    Going to add these to the headers instead.
    
    This reverts commit 0d8e539f8add6d255a7ac8ef233a7a947c9601fc.
    
    Test: ./validate.py --force-unified-headers
    Bug: None
    Change-Id: I07120f614bb3859f84718b992969ec1d5d8a75f7

--- tests/device/fenv/jni/Android.mk
+++ tests/device/fenv/jni/Android.mk
@@ -3,7 +3,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := test_fenv_c
 LOCAL_SRC_FILES := test_fenv.c
-LOCAL_STATIC_LIBRARIES := libandroid_support
+LOCAL_LDFLAGS += -lm
 include $(BUILD_EXECUTABLE)
-
-$(call import-module,android/support)
--- tests/device/issue42891-boost-1_52/jni/Android.mk
+++ tests/device/issue42891-boost-1_52/jni/Android.mk
@@ -1,11 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := sdktest
-LOCAL_SRC_FILES := main.cpp
-LOCAL_C_INCLUDES := jni/boost
-LOCAL_CPPFLAGS := -fexceptions -frtti -DCALL_X
-LOCAL_STATIC_LIBRARIES := libandroid_support
+
+LOCAL_MODULE    := sdktest
+LOCAL_SRC_FILES := main.cpp 
+LOCAL_C_INCLUDES += jni/boost
+LOCAL_CFLAGS += -fexceptions -frtti -DCALL_X
+
+#LOCAL_CFLAGS += -O2
+
 include $(BUILD_EXECUTABLE)
 
-$(call import-module,android/support)
