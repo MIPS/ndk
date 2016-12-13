ed35bbef603043968edcc6ea3af6cf5a680a4e05
Dan Albert <danalbert@google.com>
2016-12-12 23:36:46 -0800

    Fix pylint/pep8 issues.
    Test: pylint checkbuild.py
    Test: pep8 checkbuild.py
    Bug: None
    Change-Id: Iaeb43461e855e383bde3fc90ba186968272223f6

--- checkbuild.py
+++ checkbuild.py
@@ -761,13 +761,15 @@ def build_simpleperf(out_dir, dist_dir, _args):
 
     build_support.make_package('simpleperf', install_dir, dist_dir)
 
+
 def build_renderscript(out_dir, dist_dir, args):
     print('Building RenderScript...')
     # Package Android.mk for RenderScript device prebuilts.
     path = build_support.ndk_path('sources/android/renderscript')
     build_support.make_package('renderscript', path, dist_dir)
     # Package the entire RenderScript toolchain.
-    invoke_build('build-renderscript.py', common_build_args(out_dir, dist_dir, args))
+    invoke_build(
+        'build-renderscript.py', common_build_args(out_dir, dist_dir, args))
 
 
 def launch_build(build_name, build_func, out_dir, dist_dir, args, log_dir):
