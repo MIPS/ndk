e8f255ab70787a349b76aae4fe76220fc3195d7b
Dan Albert <danalbert@google.com>
2017-02-02 16:38:00 -0800

    Update sto* xfail annotations for lp64.
    The stof ERANGE issue seems to only affect lp32 ABIs.
    
    Test: ./run_tests.py --suite libc++ --abi arm64-v8a
    Test: ./run_tests.py --suite libc++ --abi armeabi-v7a
    Bug: http://b/34739876
    Change-Id: I7fde81f647695ab008d02fb5a0b094bcd9db4e24

--- tests/libc++/test/std/strings/string_conversions/test_config.py
+++ tests/libc++/test/std/strings/string_conversions/test_config.py
@@ -1,4 +1,5 @@
 def run_broken(abi, device_api, toolchain, name):
-    if name == 'stof.pass':
+    lp64_abis = ('arm64-v8a', 'mips64', 'x86_64')
+    if abi not in lp64_abis and name == 'stof.pass':
         return 'all', 'http://b/34739876'
     return None, None
