925ee091dabe8e7a41041ec56e3cb9b0177dcb4a
Dan Albert <danalbert@google.com>
2017-03-07 14:09:13 -0800

    Enable standalone toolchain tests on Windows.
    I rewrote this in Python a while ago. It should be fine to test on
    Windows now.
    
    Test: ./validate.py --filter standalone_toolchain* # on windows
    Bug: None
    Change-Id: I9564a387a111d89ba018eb27f7d8adba30d443a9

--- tests/build/standalone_toolchain_deprecated_headers/test_config.py
+++ tests/build/standalone_toolchain_deprecated_headers/test_config.py
@@ -1,21 +1,11 @@
 # Shut up a warning about us not being a real package.
 from __future__ import absolute_import
-import platform
 
 
-def build_broken(abi, api_level, toolchain):
+def build_broken(_abi, _api_level, toolchain):
     if toolchain == '4.9':
         # GCC's default include ordering is wrong, preventing the C++ stdlib
         # from overriding compiler headers. In this case we can't include
         # <cstddef> because the include_next can't find the compiler's header.
         return toolchain, 'http://b/30096326'
     return None, None
-
-
-def build_unsupported(_abi, _api_level, _toolchain):
-    if platform.system() == 'Windows':
-        # make-standalone-toolchain.sh isn't supported on Windows since we
-        # don't have bash. Note that platform.system() won't report "Windows"
-        # for cygwin, so we will run the test in that case.
-        return platform.system()
-    return None
