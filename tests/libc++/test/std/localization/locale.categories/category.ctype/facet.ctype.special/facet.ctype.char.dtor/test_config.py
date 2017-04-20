79dbafd7e55ad672d7b57748016ee328113f9915
Dan Albert <danalbert@google.com>
2017-04-20 10:12:14 -0700

    Fixup test annotations for http://b/2643900.
    Test: ./validate.py
    Bug: http://issuetracker.google.com/2643900
    Change-Id: Iec427113354518a5fa95989af4ebdd611696827e

--- tests/libc++/test/std/localization/locale.categories/category.ctype/facet.ctype.special/facet.ctype.char.dtor/test_config.py
+++ tests/libc++/test/std/localization/locale.categories/category.ctype/facet.ctype.special/facet.ctype.char.dtor/test_config.py
@@ -1,4 +1,4 @@
 def run_broken(abi, device_api, toolchain, name):
-    if name == 'dtor.pass' and device_api < 21:
+    if name == 'dtor.pass' and device_api < 18:
         return 'android-{}'.format(device_api), 'http://b/2643900'
     return None, None
