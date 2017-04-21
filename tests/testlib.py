f26e0df93c35dd9caf4249c7662bc5e6c0629bf1
Dan Albert <danalbert@google.com>
2017-04-21 14:53:49 -0700

    Check for and prevent duplicate test build dirs.
    Duplicate build directories could race. Check for them ahead of
    running the tests.
    
    Test: ./validate.py
    Bug: None
    Change-Id: I5511a6f9116466cc2d1501864c21f2c34a7b9d79
    (cherry picked from commit 653c547d7b9ebe5f4e02d54a5f649ea38e61ebab)

--- tests/testlib.py
+++ tests/testlib.py
@@ -295,16 +295,6 @@ class LibcxxTestScanner(TestScanner):
                     cls.ALL_TESTS.append(test_path)
 
 
-def _scan_test_suite(suite_dir, test_scanner):
-    tests = []
-    for dentry in os.listdir(suite_dir):
-        path = os.path.join(suite_dir, dentry)
-        if os.path.isdir(path):
-            test_name = os.path.basename(path)
-            tests.extend(test_scanner.find_tests(path, test_name))
-    return tests
-
-
 def _fixup_expected_failure(result, config, bug):
     if isinstance(result, Failure):
         return ExpectedFailure(result.test, config, bug)
@@ -461,11 +451,33 @@ class TestRunner(object):
     def __init__(self, printer):
         self.printer = printer
         self.tests = {}
+        self.build_dirs = {}
 
     def add_suite(self, name, path, test_scanner):
         if name in self.tests:
             raise KeyError('suite {} already exists'.format(name))
-        self.tests[name] = _scan_test_suite(path, test_scanner)
+        new_tests = self.scan_test_suite(path, test_scanner)
+        self.check_no_overlapping_build_dirs(name, new_tests)
+        self.tests[name] = new_tests
+
+    def scan_test_suite(self, suite_dir, test_scanner):
+        tests = []
+        for dentry in os.listdir(suite_dir):
+            path = os.path.join(suite_dir, dentry)
+            if os.path.isdir(path):
+                test_name = os.path.basename(path)
+                tests.extend(test_scanner.find_tests(path, test_name))
+        return tests
+
+    def check_no_overlapping_build_dirs(self, suite, new_tests):
+        for test in new_tests:
+            build_dir = test.get_build_dir('')
+            if build_dir in self.build_dirs:
+                dup_suite, dup_test = self.build_dirs[build_dir]
+                raise RuntimeError(
+                    'Found duplicate build directory:\n{} {}\n{} {}'.format(
+                        dup_suite, dup_test, suite, test))
+            self.build_dirs[build_dir] = (suite, test)
 
     def run(self, out_dir, test_filters):
         workqueue = wq.WorkQueue()
@@ -1497,6 +1509,9 @@ class LibcxxTest(Test):
     @property
     def skip_run(self):
         return self.config.skip_run
+
+    def get_build_dir(self, out_dir):
+        return os.path.join(out_dir, 'libcxx', str(self.config), self.name)
 
     def run(self, out_dir, test_filters):
         xunit_output = os.path.join(out_dir, 'xunit.xml')
