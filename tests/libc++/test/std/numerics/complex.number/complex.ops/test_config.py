4556ae3bd360724110cd7a9cf23685e21782d09a
Dan Albert <danalbert@google.com>
2017-04-27 21:54:40 -0700

    Remove obsolete xfail files.
    The new clang update fixes these.
    
    Test: ./validate.py
    Bug: None
    Change-Id: I4896780141499b70e9f24569a3d5d654bd698871

--- tests/libc++/test/std/numerics/complex.number/complex.ops/test_config.py
+++
@@ -1,9 +0,0 @@
-def build_broken(abi, api, toolchain, name):
-    broken_tests = (
-        'complex_divide_complex.pass',
-        'scalar_divide_complex.pass',
-    )
-    broken_abis = ('arm64-v8a', 'x86_64')
-    if abi in broken_abis and name in broken_tests:
-        return abi, 'https://github.com/android-ndk/ndk/issues/294'
-    return None, None
--- tests/libc++/test/std/numerics/complex.number/complex.transcendentals/test_config.py
+++
@@ -1,5 +0,0 @@
-def build_broken(abi, api, toolchain, name):
-    broken_abis = ('arm64-v8a', 'x86_64')
-    if abi in broken_abis and name in ('atan.pass', 'atanh.pass'):
-        return abi, 'https://github.com/android-ndk/ndk/issues/294'
-    return None, None
