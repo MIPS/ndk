079e3ede7dfeb5a65bf40fc8da96bd77b602d264
Dan Albert <danalbert@google.com>
2017-06-08 15:51:11 -0700

    Fix test broken config for strto* tests.
    Some of these have started passing for lp64 because libc++ now calls
    strto* instead of sscanf, but the strtold still has a broken nan
    parser until very recently.
    
    For lp32, since we're now calling strtof, we end up calling strtod
    because strtof was an inline function that deferred to strtod. The
    number we try to convert in the test *is* representable as a double,
    so the conversion doesn't fail.
    
    Test: ./run_tests.py
    Bug: http://b/31101647
    Bug: https://github.com/android-ndk/ndk/issues/415
    Change-Id: I02c2c23815548052850c0b2f3132f335c6cab086

--- tests/libc++/test/std/localization/locale.categories/category.numeric/locale.num.get/facet.num.get.members/test_config.py
+++ tests/libc++/test/std/localization/locale.categories/category.numeric/locale.num.get/facet.num.get.members/test_config.py
@@ -1,10 +1,12 @@
 def run_broken(abi, device_api, toolchain, name):
     lp64_abis = ('arm64-v8a', 'mips64', 'x86_64')
     failing_tests = (
-        'get_double.pass',
-        'get_float.pass',
         'get_long_double.pass',
     )
     if abi in lp64_abis and device_api < 26 and name in failing_tests:
         return 'android-{}'.format(device_api), 'http://b/31101647'
+
+    if abi not in lp64_abis and name == 'get_float.pass':
+        return abi, 'https://github.com/android-ndk/ndk/issues/415'
+
     return None, None
