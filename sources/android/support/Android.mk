39a94a921cb8fcf070e4c6aa3681c7a384608857
Dan Albert <danalbert@google.com>
2017-06-30 11:47:52 -0700

    Remove musl ctype code.
    All we need from this is iswblank, for which we can trivially provide
    our own definition.
    
    Test: ./run_tests.py
    Bug: https://github.com/android-ndk/ndk/issues/300
    Change-Id: I47ff80c27e0d475b7275c2c791a7ce1841b1a1ed

--- sources/android/support/Android.mk
+++ sources/android/support/Android.mk
@@ -1,12 +1,26 @@
 LOCAL_PATH := $(call my-dir)
 
-android_support_c_includes := $(LOCAL_PATH)/include
+android_support_export_c_includes := $(LOCAL_PATH)/include
 
 ifneq ($(filter $(NDK_KNOWN_DEVICE_ABI64S),$(TARGET_ARCH_ABI)),)
     is_lp64 := true
 else
     is_lp64 :=
 endif
+
+ifneq ($(LIBCXX_FORCE_REBUILD),true) # Using prebuilt
+
+LIBCXX_LIBS := ../../cxx-stl/llvm-libc++/libs/$(TARGET_ARCH_ABI)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android_support
+LOCAL_SRC_FILES := $(LIBCXX_LIBS)/lib$(LOCAL_MODULE)$(TARGET_LIB_EXTENSION)
+LOCAL_EXPORT_C_INCLUDES := $(android_support_export_c_includes)
+include $(PREBUILT_STATIC_LIBRARY)
+
+else # Building
+
+android_support_c_includes := $(android_support_export_c_includes)
 
 ifeq ($(is_lp64),true)
 # 64-bit ABIs
@@ -16,8 +30,23 @@ android_support_sources := \
 else
 # 32-bit ABIs
 
+BIONIC_PATH := ../../../../bionic
+
+android_support_c_includes += $(BIONIC_PATH)/libc
+
 android_support_sources := \
+    $(BIONIC_PATH)/libc/bionic/c32rtomb.cpp \
+    $(BIONIC_PATH)/libc/bionic/mbrtoc32.cpp \
+    $(BIONIC_PATH)/libc/bionic/mbstate.cpp \
+    $(BIONIC_PATH)/libc/bionic/wchar.cpp \
+    $(BIONIC_PATH)/libc/upstream-openbsd/lib/libc/locale/mbtowc.c \
     src/_Exit.cpp \
+    src/iswblank.cpp \
+    src/posix_memalign.cpp \
+    src/swprintf.cpp \
+
+# These are old sources that should be purged/rewritten/taken from bionic.
+android_support_sources += \
     src/locale/duplocale.c \
     src/locale/freelocale.c \
     src/locale/localeconv.c \
@@ -28,27 +57,10 @@ android_support_sources := \
     src/msun/e_log2.c \
     src/msun/e_log2f.c \
     src/msun/s_nan.c \
-    src/musl-ctype/iswalnum.c \
-    src/musl-ctype/iswalpha.c \
-    src/musl-ctype/iswblank.c \
-    src/musl-ctype/iswcntrl.c \
-    src/musl-ctype/iswctype.c \
-    src/musl-ctype/iswdigit.c \
-    src/musl-ctype/iswgraph.c \
-    src/musl-ctype/iswlower.c \
-    src/musl-ctype/iswprint.c \
-    src/musl-ctype/iswpunct.c \
-    src/musl-ctype/iswspace.c \
-    src/musl-ctype/iswupper.c \
-    src/musl-ctype/iswxdigit.c \
-    src/musl-ctype/wcswidth.c \
-    src/musl-ctype/wcwidth.c \
     src/musl-math/frexp.c \
     src/musl-math/frexpf.c \
     src/musl-math/frexpl.c \
-    src/posix_memalign.cpp \
     src/stdlib_support.c \
-    src/swprintf.cpp \
     src/wchar_support.c \
     src/wcstox/floatscan.c \
     src/wcstox/intscan.c \
@@ -67,20 +79,6 @@ android_support_sources += \
 endif
 
 endif  # 64-/32-bit ABIs
-
-ifneq ($(LIBCXX_FORCE_REBUILD),true) # Using prebuilt
-
-LIBCXX_LIBS := ../../cxx-stl/llvm-libc++/libs/$(TARGET_ARCH_ABI)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := android_support
-LOCAL_SRC_FILES := $(LIBCXX_LIBS)/lib$(LOCAL_MODULE)$(TARGET_LIB_EXTENSION)
-LOCAL_EXPORT_C_INCLUDES := $(android_support_c_includes)
-include $(PREBUILT_STATIC_LIBRARY)
-
-else # Building
-
-BIONIC_PATH := ../../../../bionic
 
 # This is only available as a static library for now.
 include $(CLEAR_VARS)
@@ -110,18 +108,7 @@ LOCAL_CFLAGS += \
   -Wno-shift-negative-value
 endif
 
-LOCAL_EXPORT_C_INCLUDES := $(android_support_c_includes)
-
-ifneq ($(is_lp64),true)
-LOCAL_SRC_FILES += \
-    $(BIONIC_PATH)/libc/bionic/c32rtomb.cpp \
-    $(BIONIC_PATH)/libc/bionic/mbstate.cpp \
-    $(BIONIC_PATH)/libc/bionic/mbrtoc32.cpp \
-    $(BIONIC_PATH)/libc/bionic/wchar.cpp \
-    $(BIONIC_PATH)/libc/upstream-openbsd/lib/libc/locale/mbtowc.c \
-
-LOCAL_C_INCLUDES += $(BIONIC_PATH)/libc
-endif
+LOCAL_EXPORT_C_INCLUDES := $(android_support_export_c_includes)
 
 include $(BUILD_STATIC_LIBRARY)
 
