4b92ee159a43405723e08f3b697d862458d297a2
Dan Albert <danalbert@google.com>
2017-07-19 16:44:54 -0700

    Import some ABI information from JSON.
    These JSON files were added for gradle, but we should start using
    them too.
    
    Test: nose2
    Test: ./checkbuild.py
    Bug: None
    Change-Id: Ia3192dada3f7143e81188cbba70565269219d09f

---
+++ build/test_import_abi_metadata.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Tests for import_abi_metadata.py."""
+import os
+import unittest
+
+import build.import_abi_metadata
+
+
+class ImportAbiMetadataTest(unittest.TestCase):
+    def test_generate_make_vars(self):
+        self.assertEqual(
+            'foo := bar',
+            build.import_abi_metadata.generate_make_vars(
+                {'foo': 'bar'}))
+        self.assertEqual(
+            build.import_abi_metadata.NEWLINE.join(
+                ['foo := bar', 'baz := qux']),
+            build.import_abi_metadata.generate_make_vars(
+                {'foo': 'bar', 'baz': 'qux'}))
+
+    def test_metadata_to_make_vars(self):
+        make_vars = build.import_abi_metadata.metadata_to_make_vars({
+            'armeabi': {
+                'bitness': 32,
+                'deprecated': False,
+            },
+            'armeabi-v7a': {
+                'bitness': 32,
+                'deprecated': False,
+            },
+            'arm64-v8a': {
+                'bitness': 64,
+                'deprecated': False,
+            },
+            'mips': {
+                'bitness': 32,
+                'deprecated': False,
+            },
+            'mips64': {
+                'bitness': 64,
+                'deprecated': False,
+            },
+            'x86': {
+                'bitness': 32,
+                'deprecated': False,
+            },
+            'x86_64': {
+                'bitness': 64,
+                'deprecated': False,
+            },
+        })
+
+        self.assertDictEqual({
+            'NDK_KNOWN_DEVICE_ABI32S': 'armeabi armeabi-v7a mips x86',
+            'NDK_KNOWN_DEVICE_ABI64S': 'arm64-v8a mips64 x86_64',
+        }, make_vars)
