2fd761752ac791384292b9bf26d577c4670ce21c
Dan Albert <danalbert@google.com>
2017-07-26 13:15:33 -0700

    Fix tests for GCC bugs.
    Our old GCC has Werror issues with libc++'s math.h. This causes
    failures in these tests if we fix
    https://github.com/android-ndk/ndk/issues/415, since we use stdlib.h
    in these tests and legacy_stdlib_inlines.h needs math.h for HUGE_VALF.
    
    Disable Werror for one test, and disable the test where we don't
    control the Android.mk.
    
    Test: ./run_tests.py
    Bug: https://github.com/android-ndk/ndk/issues/415
    Change-Id: I4652ca2cf0c7fb41ddb99f6561d844bd9a74a83b

--- tests/device/shaderc_libcxx/test_config.py
+++ tests/device/shaderc_libcxx/test_config.py
@@ -1,4 +1,14 @@
 def build_unsupported(abi, platform, toolchain):
     if abi == 'armeabi':
         return abi
+
+    # The shaderc makefiles set -Werror -Wattributes, and GCC is not compatible
+    # with the definition of abs in libc++'s math.h.
+    #
+    # Marking as unsupported rather than broken since GCC is deprecated so this
+    # isn't a bug (it still works fine if the user isn't using
+    # -Werror=attributes).
+    if toolchain == '4.9':
+        return toolchain
+
     return None
