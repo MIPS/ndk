d5acdb0ee5ad536d543b509d83cfa79b9d01b3b1
Dan Albert <danalbert@google.com>
2017-08-16 14:05:42 -0700

    Add _FILE_OFFSET_BITS=64 support for libc++.
    Including things like cstdio requires that we have a decl for
    everything that header needs to pull into the std namespace. We don't
    actually have these functions available in this release, but we can
    at least make this header includable.
    
    Adding a test to make sure all of the C++ C headers can be included
    in _FILE_OFFSET_BITS=64 mode.
    
    Test: ./run_tests.py
    Bug: https://github.com/android-ndk/ndk/issues/480
    Change-Id: Icfd319b293736249e5840fee7efbce1d3b69e20b

---
+++ tests/build/libcxx_headers_file_offset_bits_64/jni/Application.mk
@@ -0,0 +1,2 @@
+APP_STL := c++_static
+APP_CFLAGS := -D_FILE_OFFSET_BITS=64
---
+++ tests/build/libcxx_headers_file_offset_bits_64/jni/foo.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <ccomplex>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <cinttypes>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstdbool>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctgmath>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+
+int main(int argc, char** argv) {
+  return 0;
+}
