ad03538db2067f144049e1afb39a11afa0e77aa4
Dan Albert <danalbert@google.com>
2017-09-14 11:27:03 -0700

    Don't use TMPDIR when building toolbox.
    Test: TMPDIR=foo ./checkbuild.py --module host-tools
    Bug: None
    Change-Id: I2502bdaf48ac99ba6a0e2334d009762c9aee3e95

--- sources/host-tools/toolbox/build.py
+++ sources/host-tools/toolbox/build.py
@@ -40,6 +40,8 @@ def main(args):
     if args.host != 'windows':
         build_cmd.append('--try-64')
 
+    build_cmd.append('--build-dir=' + os.path.join(args.out_dir, 'toolbox'))
+
     build_support.build(build_cmd, args, intermediate_package=True)
 
 if __name__ == '__main__':
