4a3533d1dd9b04f962c26dd67f5bedd9c9a5ceba
Dan Albert <danalbert@google.com>
2017-09-14 11:27:03 -0700

    Don't use TMPDIR when building gdbserver.
    Test: TMPDIR=foo ./checkbuild.py --module gdbserver
    Bug: None
    Change-Id: Id20b405bcde5f929fc8e7c985d7fbde823ffa4b8

--- build/tools/build-gdbserver.sh
+++ build/tools/build-gdbserver.sh
@@ -38,10 +38,8 @@ NOTE: The --platform option is ignored if --sysroot is used."
 
 VERBOSE=no
 
-OPTION_BUILD_OUT=
-BUILD_OUT=$TMPDIR/build/gdbserver
-register_option "--build-out=<path>" do_build_out "Set temporary build directory"
-do_build_out () { OPTION_BUILD_OUT="$1"; }
+BUILD_OUT=
+register_var_option "--build-out=<path>" BUILD_OUT "Set temporary build directory"
 
 SYSROOT=
 register_var_option "--sysroot=<path>" SYSROOT "Specify sysroot directory directly"
@@ -61,7 +59,10 @@ register_try64_option
 
 extract_parameters "$@"
 
-setup_default_log_file
+if [ -z "$BUILD_OUT" ]; then
+  echo "ERROR: --build-out is required"
+  exit 1
+fi
 
 set_parameters ()
 {
@@ -166,9 +167,6 @@ PLATFORM="android-$LATEST_API_LEVEL"
 fix_sysroot "$SYSROOT"
 log "Using sysroot: $SYSROOT"
 
-if [ -n "$OPTION_BUILD_OUT" ] ; then
-    BUILD_OUT="$OPTION_BUILD_OUT"
-fi
 log "Using build directory: $BUILD_OUT"
 run rm -rf "$BUILD_OUT"
 run mkdir -p "$BUILD_OUT"
@@ -268,7 +266,7 @@ else
     DSTFILE="gdbserver"
 fi
 dump "Install  : $ARCH $DSTFILE."
-INSTALL_DIR=`mktemp -d $TMPDIR/gdbserver.XXXXXX`
+INSTALL_DIR=$BUILD_OUT/install
 GDBSERVER_SUBDIR="gdbserver-$ARCH"
 DEST=$INSTALL_DIR/$GDBSERVER_SUBDIR
 mkdir -p $DEST &&
