17f24c25442c4c8f2c328e0cdbecafd525c5bf34
Dan Albert <danalbert@google.com>
2017-09-21 06:40:41 +0000

    Revert "Set the triple propertly for libc++ tests."
    This reverts commit 4184b248f794c0530a41f3534eff541403dd2dd9.
    
    Reason for revert: broken with the current compiler.
    
    Change-Id: Iacdbf7373414f762f00fad3f54ffcc54a49b417a

--- test_libcxx.py
+++ test_libcxx.py
@@ -90,11 +90,6 @@ def main():
     triple = build.lib.build_support.arch_to_triple(arch)
     toolchain = build.lib.build_support.arch_to_toolchain(arch)
 
-    if args.abi == 'armeabi-v7a':
-        triple = 'armv7-linux-androideabi'
-
-    triple = '{}{}'.format(triple, args.platform)
-
     lit_path = build.lib.build_support.android_path(
         'external/llvm/utils/lit/lit.py')
 
