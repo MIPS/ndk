3a022f9b5e6e2f362ff8ac251cbf6c24cc3024ac
Dan Albert <danalbert@google.com>
2017-12-06 14:00:21 -0800

    Remove explicit fuse-ld=gold in flto test.
    The new Clang supports LTO via bfd as well.
    
    Test: ./run_tests.py --rebuild --filter flto
    Bug: None
    Change-Id: I081875639531949924f4c17f6bbf32ff60de61b7

--- tests/build/flto/jni/Android.mk
+++ tests/build/flto/jni/Android.mk
@@ -13,11 +13,4 @@ LOCAL_STATIC_LIBRARIES := libbar
 LOCAL_CFLAGS += -flto
 LOCAL_LDFLAGS += -flto
 
-# Clang LTO is only supported with gold. ARM64 still uses bfd by default, so
-# make sure this test uses gold when we're using clang.
-ifneq ($(filter clang%,$(NDK_TOOLCHAIN_VERSION)),)
-LOCAL_CFLAGS += -fuse-ld=gold
-LOCAL_LDFLAGS += -fuse-ld=gold
-endif
-
 include $(BUILD_EXECUTABLE)
