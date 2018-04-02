7cedb255e29f5c698eee76d0d42407ebce830099
Dan Albert <danalbert@google.com>
2018-04-02 14:22:38 -0700

    Uncanary the build.
    Test: ./checkbuild.py
    Bug: None
    Change-Id: I04d579eeeb7f08804efb966fac4a3d77a43007c3

--- ndk/config.py
+++ ndk/config.py
@@ -3,7 +3,7 @@ hotfix = 0
 hotfix_str = chr(ord('a') + hotfix) if hotfix else ''
 beta = 2
 beta_str = '-beta{}'.format(beta) if beta > 0 else ''
-canary = True
+canary = False
 canary_str = '-canary' if canary else ''
 release = 'r{}{}{}{}'.format(major, hotfix_str, beta_str, canary_str)
 if __name__ == '__main__':
