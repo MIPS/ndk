17c3d2c7142108dd4e76bda5cc3cc795336e2403
Dan Albert <danalbert@google.com>
2018-04-16 13:25:12 -0700

    Update config to r17-canary.
    Test: ./checkbuild.py
    Bug: None
    Change-Id: If2bc5b24c7615a0ea513c9707125f1fe292a61f3

--- ndk/config.py
+++ ndk/config.py
@@ -1,9 +1,9 @@
 major = 17
 hotfix = 0
 hotfix_str = chr(ord('a') + hotfix) if hotfix else ''
-beta = 2
+beta = 0
 beta_str = '-beta{}'.format(beta) if beta > 0 else ''
-canary = False
+canary = True
 canary_str = '-canary' if canary else ''
 release = 'r{}{}{}{}'.format(major, hotfix_str, beta_str, canary_str)
 if __name__ == '__main__':
